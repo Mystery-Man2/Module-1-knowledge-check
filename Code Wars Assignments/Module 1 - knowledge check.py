7779f26f6ee4777458dc0668a6537a1904f5183b
Thad Howell <howell.thadj@gmail.com>
2024-11-08 18:12:20 -0600

    module 1 knowledge check

---
+++ Code Wars Assignments/Module 1 - knowledge check.py
@@ -0,0 +1,21 @@
+# Even or Odd
+def even_or_odd(number):
+    if (number % 2) == 0:
+        return ("Even")
+    else:
+        return ("Odd")
+        
+        
+        
+# Convert a Number to a String 
+def number_to_string(num):
+    number_to_string = str("")
+    return str(num)
+
+
+# Vowel  Count 
+def get_count(sentence):
+    count_vowels = 0
+    for vowels in ['a', 'e', 'i', 'o', 'u']:
+        count_vowels += sentence.count(vowels)
+    return count_vowels
